3660817ca1ae719149c01350c854c266e5289944
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-05-24 01:01:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+             <h4 className='text-center text-cyan-800 text-8xl font-extrabold'>Hello world</h4>
+  </div>
+  )
+}
+
+export default App

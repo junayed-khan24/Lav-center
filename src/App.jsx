6aa43cf146f16202bc61f972ca4e6ebb3c48da2e
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-05-25 21:51:19 +0600

    added navber.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Navber from './Navber'
 
 function App() {
 
   return (
   <div>
+    <Navber></Navber>
              <h4 className='text-center text-cyan-800 text-8xl font-extrabold'>Hello world</h4>
   </div>
   )
